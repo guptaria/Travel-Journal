0ea7a12b211a036336f74d08e6db8294fe31860d
thamma <thammaraku@gmail.com>
2021-01-29 01:31:13 -0600

    able to add place and render on map

--- public/js/map.js
+++ public/js/map.js
@@ -6,45 +6,50 @@
 let map;
 let service;
 let infowindow;
-var userListObj = [];
+// var userListArr = [];
+// var userListObj = [{
+//   "place": "franklin bbq",
+//   "latlang": (30.2701188, -97.7313156)
+// }];
 
-var userListObj = [{
-  "place": "franklin bbq",
-  "latlang": (30.2701188, -97.7313156)
-}];
-
-// var map = new google.maps.Map(document.getElementById("map"));
-
+const userListArr = [];
+// const userListArr = [
+//   ["Texas State Capitol", 30.2747, -97.7404],
+//   ["Barton Spring Pool", 30.264, -97.771],
+//   ["Frankin BBQ", 30.2701, -97.7313],
+//   ["The Oasis on Lake Travis", 30.4057, -97.8741]
+// ];
 
 function initMap() {
+  console.log("userListArr = " + userListArr);
   // user location input has to be in this format: arrays of arrays
-  const locations = [
-    ["Texas State Capitol", 30.2747, -97.7404],
-    ["Barton Spring Pool", 30.264, -97.771],
-    ["Frankin BBQ", 30.2701, -97.7313],
-    ["The Oasis on Lake Travis", 30.4057, -97.8741]
-  ];
+  // const locations = [
+  //   ["Texas State Capitol", 30.2747, -97.7404],
+  //   ["Barton Spring Pool", 30.264, -97.771],
+  //   ["Frankin BBQ", 30.2701, -97.7313],
+  //   ["The Oasis on Lake Travis", 30.4057, -97.8741]
+  // ];
 
   // Always center the first location
-  const userLocation1LaLong = new google.maps.LatLng(
-    locations[0][1],
-    locations[0][2]
-  );
+  // const userLocation1LaLong = new google.maps.LatLng(
+  //   userListArr[0][1],
+  //   userListArr[0][2]
+  // );
 
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 13,
+    zoom: 4,
     // center: { lat: -25.344, lng: 131.036 },
-    // center: new google.maps.LatLng(49.343085, -123.305938),
-    center: userLocation1LaLong
+    center: new google.maps.LatLng(37.0902, -95.7129)
+    // center: userLocation1LaLong
   });
 
   // Drop pins on all locations
   const latlngbounds = new google.maps.LatLngBounds();
-  for (i = 0; i < locations.length; i++) {
+  for (i = 0; i < userListArr.length; i++) {
     // const userLocation1LaLong = new google.maps.LatLng(locations[0][1], locations[0][2]);
     const markerLocation = new google.maps.LatLng(
-      locations[i][1],
-      locations[i][2]
+      userListArr[i][1],
+      userListArr[i][2]
     );
 
     // eslint-disable-next-line no-unused-vars
@@ -57,11 +62,12 @@ function initMap() {
     latlngbounds.extend(markerLocation);
   }
   map.fitBounds(latlngbounds);
-
 }
 
 
 function getGeolocation(placeName) {
+  // console.log("userListArr" + userListArr);
+
   infowindow = new google.maps.InfoWindow();
   var map = new google.maps.Map(document.getElementById("map"));
 
@@ -95,6 +101,19 @@ function getGeolocation(placeName) {
 
         const geoLocationArr = [placeName, latitude, longtitude]
         console.log("geoLocationArr = " + geoLocationArr);
+
+        // Add new place into existing array
+        userListArr.push(geoLocationArr);
+        // console.log("userListArr = " + userListArr);
+        console.log("userListArr[0] = " + userListArr[0]);
+        console.log("userListArr[1] = " + userListArr[1]);
+        console.log("userListArr[2] = " + userListArr[2]);
+        console.log("userListArr[3] = " + userListArr[3]);
+        console.log("userListArr[4] = " + userListArr[4]);
+        
+        renderJournal(userListArr);
+        initMap(userListArr);
+
         // console.log("geoLocationArr[0]" + geoLocationArr[0]);
         // console.log("geoLocationArr[1]" + geoLocationArr[1]);
         // console.log("geoLocationArr[2]" + geoLocationArr[2]);
@@ -115,13 +134,12 @@ function getGeolocation(placeName) {
 
 
 function renderJournal() {
-  // var searchHistoryObj = JSON.parse(localStorage.getItem('searchHistory'));
-  if (userListObj) {
-    for (var i = 0; i < userListObj.length; ++i) {
-      $(`#row${i}`).html(`<td><button class="recent btn btn-link">${userListObj[i].place}}</button></td>`);
+  console.log("userListArr = " + userListArr);
+  if (userListArr) {
+    for (var i = 0; i < userListArr.length; ++i) {
+      $(`#row${i}`).html(`<td><button class="recent btn btn-link">${userListArr[i][0]}</button></td>`);
     }
   }
-
 }
 
 
@@ -139,21 +157,15 @@ function renderJournal() {
 //////////////////////////// EXECUTION ////////////////////////////////////////
 
 $(document).ready(function () {
-
   renderJournal();
-  initMap();
+  initMap(userListArr);
 
   $("#searchBtn").click(function (event) {
     event.preventDefault;
-
     // WORKING SKIPPED FOR NOW TO SAVE API CALL
     var placeName = $('input').val();
     getGeolocation(placeName);
-
-
-
   })
-
 });
 
 
@@ -181,3 +193,48 @@ $(document).ready(function () {
   //     map.setCenter(results[0].geometry.location);
   //   }
   // });
+
+
+  // function initMap() {
+  //   // user location input has to be in this format: arrays of arrays
+  //   const locations = [
+  //     ["Texas State Capitol", 30.2747, -97.7404],
+  //     ["Barton Spring Pool", 30.264, -97.771],
+  //     ["Frankin BBQ", 30.2701, -97.7313],
+  //     ["The Oasis on Lake Travis", 30.4057, -97.8741]
+  //   ];
+  
+  //   // Always center the first location
+  //   const userLocation1LaLong = new google.maps.LatLng(
+  //     locations[0][1],
+  //     locations[0][2]
+  //   );
+  
+  //   const map = new google.maps.Map(document.getElementById("map"), {
+  //     zoom: 13,
+  //     // center: { lat: -25.344, lng: 131.036 },
+  //     // center: new google.maps.LatLng(49.343085, -123.305938),
+  //     center: userLocation1LaLong
+  //   });
+  
+  //   // Drop pins on all locations
+  //   const latlngbounds = new google.maps.LatLngBounds();
+  //   for (i = 0; i < locations.length; i++) {
+  //     // const userLocation1LaLong = new google.maps.LatLng(locations[0][1], locations[0][2]);
+  //     const markerLocation = new google.maps.LatLng(
+  //       locations[i][1],
+  //       locations[i][2]
+  //     );
+  
+  //     // eslint-disable-next-line no-unused-vars
+  //     const marker = new google.maps.Marker({
+  //       // position: { lat: -25.344, lng: 131.036 },
+  //       position: markerLocation,
+  //       map: map
+  //     });
+  //     console.log("markerLocation = " + markerLocation);
+  //     latlngbounds.extend(markerLocation);
+  //   }
+  //   map.fitBounds(latlngbounds);
+  
+  // }
