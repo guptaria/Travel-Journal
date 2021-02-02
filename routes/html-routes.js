7dca8f9f16767aa09adaa613358d6c21eaa19499
Christian <927.christian@gmail.com>
2021-02-01 21:19:23 -0600

    modified:   routes/html-routes.js

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,40 +8,52 @@ module.exports = function(app) {
 
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    console.log(req.user);
     if (req.user) {
       res.redirect("/home");
     }
     // res.sendFile(path.join(__dirname, "../public/sign-up.html"));
     res.sendFile(path.join(__dirname, "../public/google-signup.html"));
+    //res.sendFile(path.join(__dirname, "../public/home.html"));
     // res.sendFile(path.join(__dirname, "../public/user_journal.html"));
   });
 
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
-    console.log(req.user);
     if (req.user) {
-      res.redirect("/home");
+      res.redirect("/explore");
     }
     // res.sendFile(path.join(__dirname, "../public/log-in.html"));
     res.sendFile(path.join(__dirname, "../public/google-login.html"));
   });
 
-  // Here we've add our isAuthenticated middleware to this route.
-  // If a user who is not logged in tries to access this route they will be redirected to the signup page
+  app.get("/sign-up", function(req, res) {
+    // If the user already has an account send them to the members page
+    // if (req.user) {
+    //   res.redirect("/home.html");
+    // }
+    // res.sendFile(path.join(__dirname, "../public/log-in.html"));
+    res.sendFile(path.join(__dirname, "../public/google-signup.html"));
+  });
+
+  //Here we've add our isAuthenticated middleware to this route.
+  //If a user who is not logged in tries to access this route they will be redirected to the signup page
   // app.get("/members", isAuthenticated, function(req, res) {
   //   res.sendFile(path.join(__dirname, "../public/members.html"));
   // });
 
   // Route to explore page
   app.get("/explore", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/explore.html"));
+    res.sendFile(path.join(__dirname, "../public/user-home.html"));
   });
-  // Routes to user page
-  app.get("/home", function(req, res) {
-    // res.sendFile(path.join(__dirname, "../public/home.html"));
-    res.sendFile(path.join(__dirname, "../public/user_journal.html"));
+  
+  //routes to primary dashboard user page
+  app.get("/my-journals", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/main-journal.html"));
+  });
 
+  // Routes to add a new journal
+  app.get("/journal-entry", isAuthenticated, function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/user_journal.html"));
   });
 
 };
