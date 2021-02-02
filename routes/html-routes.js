0a5c23dd09a21162523ac29a86e88f01c4be7325
Priya Garg <gupta.ria100@gmail.com>
2021-02-01 21:05:37 -0600

    modifiing databes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -8,16 +8,17 @@ module.exports = function(app) {
 
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/home");
-    }
+    // if (req.user) {
+    //   res.redirect("/home");
+    // }
+    console.log(req.user);
     // res.sendFile(path.join(__dirname, "../public/sign-up.html"));
-    res.sendFile(path.join(__dirname, "../public/google-signup.html"));
-    // res.sendFile(path.join(__dirname, "../public/user_journal.html"));
+    // res.sendFile(path.join(__dirname, "../public/google-signup.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   app.get("/login", function(req, res) {
-    // If the user already has an account send them to the members page
+    // If the user already has an account send them to the google-login page
     if (req.user) {
       res.redirect("/home");
     }
@@ -25,6 +26,18 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../public/google-login.html"));
   });
 
+  app.get("/home", function(req, res) {
+    // If the user already has an account send them to the google-login page
+   
+    // res.sendFile(path.join(__dirname, "../public/log-in.html"));
+    res.sendFile(path.join(__dirname, "../public/user-home.html"));
+  });
+  app.get("/journal", function(req, res) {
+    // If the user already has an account send them to the google-login page
+   
+    // res.sendFile(path.join(__dirname, "../public/log-in.html"));
+    res.sendFile(path.join(__dirname, "../public/user-journal.html"));
+  });
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   // app.get("/members", isAuthenticated, function(req, res) {
