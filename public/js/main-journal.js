a48187dba187db76c69e8d98b2258784435f7c1d
Priya Garg <gupta.ria100@gmail.com>
2021-02-02 21:13:11 -0600

    fixed broken code

--- public/js/main-journal.js
+++ public/js/main-journal.js
@@ -2,7 +2,6 @@
 // var UserId = document.cookie;
 //   console.log(UserId);
 
-<<<<<<< HEAD
 // $(document).ready(function () {
 //   var h3 = $(".titleThis");
 //   var h4 = $("<h4>");
@@ -26,7 +25,6 @@
 //       }
 //     });
 //   }
-=======
 $(document).ready(function () {
   
   const journalSection = $(`section.Journal`);
@@ -68,92 +66,22 @@ $(document).ready(function () {
         });
 
 
-
-
-        // If this post exists, prefill our cms forms with its data
-        // postCategorySelect.val(data.location);
-        // If we have a post with this id, set a flag for us to know to update the post
-        // when we hit submit
-                // updating = true;
-      }
-    });
-  }
->>>>>>> a3be331ec89955ba8260b0a5a9c07e4b216b3188
- 
-//     $.get("/api/user_data", function (data) {
-//       console.log(data);
-//       // make sure you are grabbing the correct id from the console log above
-//       const UserId = data.UserId;
-//       console.log(UserId);
-//       // now run the call with the userId
-//       getJournalData(UserId);
-//     });
   
-// });
-
-//Toggle on Journal Entry//
-
-
-
-// $(document).ready(function() {
-//     $('#entry').on('click', function() {
-//         $('.show-description').toggleClass('.show-description');
-//     });
-// });
-
-
-// $('#entry').hide();
-
 // $(document).ready(function(){
+//    $('p').hide();
+//    $('h3').click(function (){
+//        $('p').hide();
+//        $(this).parent().children('p').toggle();
+//      });
 
-//     $("#thailand").click(function(){
-//       $('p').toggleClass('paragraph toggle');
-//     });
-   
+//    $('.journalButtons').hide();
+//    $('h3').click(function (){
+//        $('.journalButtons').hide();
+//        $(this).parent().children('.journalButtons').toggle();
+//      });
+
+//     //  $('h3').click(function (){
+//     //      $('p').toggle();
+//     //  });
 //    });
-
-// $(document).ready(function() {
-//     $('p').hide();
-// });
-
-// let ...  = 
-
-// $(document).ready(function() {
-// $('#mexico-city').on('click', function() {
-
-//     $("p").toggleClass();
-//     });
-
-// $('.entry').on('click', function() {
-//         $("p").toggleClass('paragraph',true);
-
-
-//     });
-// });
-
-
-
-
-
-
-  // Getting a reference to the input field where user adds a new todo
-  //   
-  
-$(document).ready(function(){
-   $('p').hide();
-   $('h3').click(function (){
-       $('p').hide();
-       $(this).parent().children('p').toggle();
-     });
-
-   $('.journalButtons').hide();
-   $('h3').click(function (){
-       $('.journalButtons').hide();
-       $(this).parent().children('.journalButtons').toggle();
-     });
-
-    //  $('h3').click(function (){
-    //      $('p').toggle();
-    //  });
-   });
 
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -77,30 +77,31 @@ module.exports = function(app) {
     // });
 
     // Route to explore page
-    app.get('/explore', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/user-home.html'));
-    });
+    // app.get('/explore', function (req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/user-home.html'));
+    // });
 
-    //routes to primary dashboard user page
-    app.get('/my-journals', isAuthenticated, function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/main-journal.html'));
-    });
+    // //routes to primary dashboard user page
+    // app.get('/my-journals', isAuthenticated, function (req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/main-journal.html'));
+    // });
 
-    // Routes to add a new journal
-    app.get('/journal-entry', isAuthenticated, function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/user_journal.html'));
-    });
+    // // Routes to add a new journal
+    // app.get('/journal-entry', isAuthenticated, function (req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/user_journal.html'));
+    // });
 
-    // Route to explore page
-    app.get('/explore', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/user-home.html'));
-    });
-    // Routes to user page
-    app.get('/home', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/home.html'));
-    });
-    // Routes to journal page
-    app.get('/journal', function (req, res) {
-        res.sendFile(path.join(__dirname, '../public/main-journal.html'));
-    });
-};
+    // // Route to explore page
+    // app.get('/explore', function (req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/user-home.html'));
+    // });
+    // // Routes to user page
+    // app.get('/home', function (req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/home.html'));
+    // });
+    // // Routes to journal page
+    // app.get('/journal', function (req, res) {
+    //     res.sendFile(path.join(__dirname, '../public/main-journal.html'));
+    // });
+
+
